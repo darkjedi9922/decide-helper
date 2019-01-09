bcfc04a1336824c07d14caaf98dc6462799d8bcd
Jed <darkjedi9922@gmail.com>
2019-01-09 14:30:22 +0200

    Удаляет свойство update из Session.
    Оно так и не пригодилось.

--- dhelper/tbot/base.py
+++ dhelper/tbot/base.py
@@ -11,7 +11,7 @@ class Session:
     @classmethod
     def new(cls, update):
         chatId = update.effective_chat.id
-        Session._instances[chatId] = Session(update)
+        Session._instances[chatId] = Session()
 
     @classmethod
     def get(cls, update):
@@ -19,7 +19,6 @@ class Session:
         if chatId not in Session._instances:
             return None
         session = Session._instances[chatId]
-        session.setUpdate(update)
         return session
 
     @classmethod
@@ -35,14 +34,10 @@ class Session:
         if chatId in Session._instances:
             del Session._instances[chatId]
 
-    def __init__(self, update):
+    def __init__(self):
         self._net = dhelper.Net()
-        self.setUpdate(update)
-        
         self.activeGenerator = None
 
     def getNet(self):
         return self._net
 
-    def setUpdate(self, update):
-        self._update = update
